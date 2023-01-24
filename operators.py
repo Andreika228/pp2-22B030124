448ee264fca42837b9f7de6fd2c5904c1b435e1d
Andreika228 <a_golovinov@kbtu.kz>
2023-01-24 07:17:44 +0300

    tsis1

---
+++ operators.py
@@ -0,0 +1,4 @@
+print(10+5)
+print(3*8)
+print(20%3)
+print(2**5)
---
+++ strings.py
@@ -0,0 +1,19 @@
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+  txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+  a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
