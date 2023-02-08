440ce1afbd36d71e670b9cfd29e8936f23094201
Andreika228 <a_golovinov@kbtu.kz>
2023-02-08 20:20:44 +0300

    Adding details

--- tsis3/3_5.py
+++ tsis3/3_5.py
@@ -1,39 +1,40 @@
 class Account:
-    def __init__(self,owner,balance): 
-        self.owner=owner
+    def __init__(self,user,balance): 
+        self.user=user
         self.balance=balance
         self.dep=0
     def withdraw(self,howmuch):
         if howmuch>self.balance:
-            print(f"{self.owner}, You have too little money")
+            print(f"{self.user}, You have too little money")
         else:
             self.balance=self.balance-howmuch
-            print(f"{self.owner}, your certain balance: {self.balance}")
+            print(f"{self.user}, your balance is: {self.balance}")
     def deposit(self, howmuch):
         if howmuch>self.balance:
-            print(f"{self.owner}, You have too little money")
+            print(f"{self.user}, You have too little money")
         else: 
             self.balance=self.balance-howmuch
             self.dep=self.dep+howmuch
-            print(f"Your deposit replenished on {howmuch}\n{self.owner}, your certain balance: {self.balance}")
+            print(f"Your deposit replenished on {howmuch}\n{self.user}, your balance is: {self.balance}")
 
-print("Hello,lets create a new bank account")
+print("Hello,lets create a bank account")
 x=input("Enter your name: ")
 y=int(input("Enter your balance: "))
 acc=Account(x,y)
-while True:
+m= True
+while m:
     c=input("What do you want to do: (Balance) (Deposit) (Withdraw) (Exit): ")
     if c=='Balance':
-        print(f"{acc.owner}, your certain balance: {acc.balance}")
+        print(f"{acc.user}, your balance is: {acc.balance}")
     elif c=='Deposit':
-        d=int(input(f"{acc.owner}, how much do you want to top up your deposit? "))
+        d=int(input(f"{acc.user}, how much do you want to top up your deposit? "))
         acc.deposit(d)
     elif c=='Withdraw':
-        d=int(input(f"{acc.owner}, how much do you want to withdraw? "))
+        d=int(input(f"{acc.user}, how much do you want to withdraw? "))
         acc.withdraw(d)
     elif c=='Exit':
-        print(f'{acc.owner}, we are still waiting for you, goodbye')
-        break
+        print(f'{acc.user}, Thank you for using our banking system, come again, goodbye')
+        m=False
     else:
         print('''You have only 3 options:
 (Deposit)
