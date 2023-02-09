1f6d7762dd75a75a146c0b81081a0574272b5fb8
Andreika228 <a_golovinov@kbtu.kz>
2023-02-09 09:55:00 +0300

    Adding details

--- tsis3/3_5.py
+++ tsis3/3_5.py
@@ -3,19 +3,19 @@ class Account:
         self.owner=owner
         self.balance=balance
         self.dep=0
-    def withdraw(self,howmuch):
-        if howmuch>self.balance:
-            print(f"{self.owner}, You have too little money")
+    def withdraw(self,summa):
+        if summa>self.balance:
+            print(self.owner, "You have too little money")
         else:
-            self.balance=self.balance-howmuch
-            print(f"{self.owner}, your balance is: {self.balance}")
-    def deposit(self, howmuch):
-        if howmuch>self.balance:
-            print(f"{self.owner}, You have too little money")
+            self.balance=self.balance-summa
+            print(self.owner, "your balance is:",self.balance)
+    def deposit(self, summa):
+        if summa>self.balance:
+            print(self.owner, "You have too little money")
         else: 
-            self.balance=self.balance-howmuch
-            self.dep=self.dep+howmuch
-            print(f"Your deposit replenished on {howmuch}\n{self.owner}, your balance is: {self.balance}")
+            self.balance=self.balance-summa
+            self.dep=self.dep+summa
+            print( "Your deposit replenished on",summa,"\n",self.owner, "your balance is:", self.balance,)
 
 x=Account("Andrey",5000)
 x.withdraw(2000)
