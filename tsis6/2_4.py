369b419837e4106f6e3332614a8a69155d739f0b
Andreika228 <a_golovinov@kbtu.kz>
2023-03-07 10:01:35 +0300

    Tsis6 4 task Files and Directories

--- tsis6/2_4.py
+++ tsis6/2_4.py
@@ -1,5 +1,5 @@
 lines =0
-with open ('Tester.docx',encoding="utf-8") as file:
+with open (r'C:\Users\ПК\Desktop\Tester.txt', 'r') as file:
     for line in file:
         lines+=1
 print("Number of lines: ", lines)
